dba944ba0e7872fdeb84773517ef2d465ba01e5a
unknown <ibrahimabuisamilksach@gmail.com>
2024-10-07 14:14:35 +0300

    fix

--- app/Http/Controllers/BeneficialController.php
+++ app/Http/Controllers/BeneficialController.php
@@ -24,6 +24,7 @@ class BeneficialController extends Controller
     public function store(StoreBeneficialRequest $request)
     {
 
+
         // تحقق مما إذا كانت بيانات العائلة موجودة مسبقاً
         if ($this->isBeneficialExists($request->id_num,$request->wife_id_num)) {
 
@@ -122,6 +123,7 @@ class BeneficialController extends Controller
     {
 
         $beneficial = WordFood::where('id_num', $id_num)->first();
+
         if($beneficial){
             $this->createUser($request);
 
@@ -135,35 +137,69 @@ class BeneficialController extends Controller
                 'mobile' => $request->mobile,
             ]);
 
-            // تحديث تفاصيل الأسرة
-            FamilyDetailsInfo::where('beneficial_id', $beneficial->id)->update([
-                'province' => $request->province,
-                'city' => $request->city,
-                'housing_complex' => $request->housing_complex,
-                'neighborhood' => $request->neighborhood,
-                'street' => $request->street,
-                'nearest_landmark' => $request->nearest_landmark,
-                'is_displaced' => $request->is_displaced,
-                'is_owner' => $request->is_owner,
-                'housing_type' => $request->housing_type,
-                'war_damage' => $request->war_damage,
-                'damage_type' => $request->damage_type,
-                'male_count' => $request->male_count,
-                'female_count' => $request->female_count,
-                'children_under_2' => $request->children_under_2,
-                'children_under_3' => $request->children_under_3,
-                'children_5_to_16' => $request->children_5_to_16,
-                'document' => $request->document,
-                'is_breadwinner_disabled' => $request->is_breadwinner_disabled,
-                'has_disability' => $request->has_disability,
-                'disability_type' => $request->disability_type,
-                'has_chronic_disease' => $request->has_chronic_disease,
-                'war_victim' => $request->war_victim,
-                'income_source' => $request->income_source,
-                'average_income' => $request->average_income,
-                'is_employee' => $request->is_employee,
-                'marital_status' => $request->marital_status,
-            ]);
+            // // تحديث تفاصيل الأسرة
+            // FamilyDetailsInfo::where('beneficial_id', $beneficial->id)->update([
+            //     'province' => $request->province,
+            //     'city' => $request->city,
+            //     'housing_complex' => $request->housing_complex,
+            //     'neighborhood' => $request->neighborhood,
+            //     'street' => $request->street,
+            //     'nearest_landmark' => $request->nearest_landmark,
+            //     'is_displaced' => $request->is_displaced,
+            //     'is_owner' => $request->is_owner,
+            //     'housing_type' => $request->housing_type,
+            //     'war_damage' => $request->war_damage,
+            //     'damage_type' => $request->damage_type,
+            //     'male_count' => $request->male_count,
+            //     'female_count' => $request->female_count,
+            //     'children_under_2' => $request->children_under_2,
+            //     'children_under_3' => $request->children_under_3,
+            //     'children_5_to_16' => $request->children_5_to_16,
+            //     'document' => $request->document,
+            //     'is_breadwinner_disabled' => $request->is_breadwinner_disabled,
+            //     'has_disability' => $request->has_disability,
+            //     'disability_type' => $request->disability_type,
+            //     'has_chronic_disease' => $request->has_chronic_disease,
+            //     'war_victim' => $request->war_victim,
+            //     'income_source' => $request->income_source ?? '0',
+            //     'average_income' => $request->average_income ?? 0,
+            //     'is_employee' => $request->is_employee ?? '0',
+            //     'marital_status' => $request->marital_status,
+            // ]);
+
+            // Create or update family details
+            FamilyDetailsInfo::updateOrCreate(
+                ['beneficial_id' => $beneficial->id], // criteria for finding the record
+                [
+                    'province' => $request->province,
+                    'city' => $request->city,
+                    'housing_complex' => $request->housing_complex,
+                    'neighborhood' => $request->neighborhood,
+                    'street' => $request->street,
+                    'nearest_landmark' => $request->nearest_landmark,
+                    'is_displaced' => $request->is_displaced,
+                    'is_owner' => $request->is_owner,
+                    'housing_type' => $request->housing_type,
+                    'war_damage' => $request->war_damage,
+                    'damage_type' => $request->damage_type,
+                    'male_count' => $request->male_count,
+                    'female_count' => $request->female_count,
+                    'children_under_2' => $request->children_under_2,
+                    'children_under_3' => $request->children_under_3,
+                    'children_5_to_16' => $request->children_5_to_16,
+                    'document' => $request->document,
+                    'is_breadwinner_disabled' => $request->is_breadwinner_disabled,
+                    'has_disability' => $request->has_disability,
+                    'disability_type' => $request->disability_type ?? '-',
+                    'has_chronic_disease' => $request->has_chronic_disease,
+                    'war_victim' => $request->war_victim,
+                    'income_source' => $request->income_source ?? '0',
+                    'average_income' => $request->average_income ?? 0,
+                    'is_employee' => $request->is_employee ?? '0',
+                    'marital_status' => $request->marital_status,
+                ]
+            );
+
         }
     }
 
--- routes/admin.php
+++ routes/admin.php
@@ -67,7 +67,7 @@ Route::get('beneficial/create-form', [BeneficialController::class,'createNew']);
 // _______________________________
 Route::get('profile', [AdminController::class, 'Profile'])->name('admin.profile.view');
 Route::post('profile_store', [AdminController::class, 'Profile_update'])->name('admin.profile.store');
-// Route::get('import', [HomeAdminController::class, 'import'])->name('admin.import');
+ Route::get('import2', [HomeAdminController::class, 'import'])->name('admin.import');
 Route::post('import', [HomeAdminController::class, 'importNewIds'])->name('admin.importNewIds');
 
 Route::get('import-res', [HomeAdminController::class, 'importRes'])->name('admin.import_res');
