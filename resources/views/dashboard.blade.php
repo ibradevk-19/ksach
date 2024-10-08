43c44c4edd7f0202e44a2f9536fed7f4c21dccde
unknown <ibrahimabuisamilksach@gmail.com>
2024-10-08 10:34:24 +0300

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,153 +1,184 @@
-<!DOCTYPE html>
-<html lang="ar">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>لوحة التحكم</title>
-    <!-- Bootstrap RTL CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
+<!doctype html>
+<html lang="en" dir="rtl" >
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>المركز السعودي للثقافة و التراث | Saudi Center for Culture and Heritage</title>
+    <!-- CSS files -->
+    <link href="{{ asset('frontend/dist/css/tabler.rtl.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('frontend/dist/css/tabler-flags.rtl.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('frontend/dist/css/tabler-payments.rtl.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('frontend/dist/css/tabler-vendors.rtl.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('frontend/dist/css/demo.rtl.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="http://www.fontstatic.com/f=jazeera,jazeera-light" rel="stylesheet">
+
     <style>
-        /* Styling for the dashboard */
-        body {
-            font-family: 'Cairo', sans-serif;
-            direction: rtl; /* Set RTL direction */
-            text-align: right; /* Align text to the right */
-        }
 
-        .container-fluid {
-            padding: 20px;
-        }
-
-        .navbar-brand {
-            font-size: 1.5rem;
-        }
-
-        /* Ensuring a bit of margin between cards and responsiveness */
-        .card {
-            margin-bottom: 20px;
-        }
+      @import url('http://www.fontstatic.com/f=jazeera,jazeera-light');
+      :root {
+      	font-family: 'jazeera-light';
+      }
+      body * {
+        font-family: 'jazeera-light';
+      }
     </style>
-</head>
-<body>
+  </head>
+  <body >
+    <script src="./dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page">
+      <!-- Navbar -->
+      <header class="navbar navbar-expand-md d-print-none" >
+        <div class="container-xl">
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">المركز السعودي للثقافة و التراث</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+          <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+            <a href=".">
+              <img src="./static/logo.svg" width="110" height="32" alt="المركز السعودي للثقافة و التراث" class="navbar-brand-image">
+            </a>
+          </h1>
+          <div class="navbar-nav flex-row order-md-last">
+            <div class="nav-item d-none d-md-flex me-3">
+              <div class="btn-list">
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-                <!-- Display User's Name -->
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <!-- Display the authenticated user's name -->
-                            {{ Auth::user()->name }} <!-- اسم المستخدم -->
 
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><hr class="dropdown-divider"></li>
-                            <!-- Logout functionality -->
-                            <li>
-                                <form action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button class="dropdown-item" type="submit">تسجيل الخروج</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+              </div>
             </div>
-        </div>
-    </nav>
+            <div class="d-none d-md-flex">
 
-    <!-- Main content -->
-    <div class="container-fluid ">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 mb-4">
-                <h1 class="mt-4 text-center mb-4">مرحباً بك في المركز السعودي للثقافة و الثرات </h1>
-                <p class="text-center mb-4">هذه هي لوحة التحكم الخاصة بك. يمكنك الاطلاع على موعد الدورة الخاصة بك و تعديل بيانات الاسرة و الاطفال و تعديل المندوب الخاص بك.</p>
+
             </div>
-        </br></br></br>
-        </div>
-        <!-- Dashboard Cards -->
-        <div class="row">
-            <!-- Responsive Columns for small devices -->
-            <div class="col-md-4 col-sm-6">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h5 class="card-title">تعديل البيانات الاسرى </h5>
-                        <a href="{{ route('dashboard.user.index') }}" class="btn btn-primary">تعديل </a>
-                    </div>
+            <div class="nav-item dropdown">
+              <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
+                <span class="avatar avatar-sm" style="background-image: url(./dist/img/menu-2.png)"></span>
+                <div class="d-none d-xl-block ps-2">
+                  <div>{{ Auth::user()->name }}    </div>
+                  <div class="mt-1 small text-secondary">{{ Auth::user()->id_number }}</div>
                 </div>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h5 class="card-title">  اضافة بيانات الاطفال </h5>
-                        <a href="#" class="btn btn-primary">اضافة </a>
-                    </div>
-                </div>
-            </div>
+              </a>
+              <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                <a href="" class="dropdown-item">تغير كلمة المرور</a>
+                <div class="dropdown-divider"></div>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="dropdown-item" type="submit">تسجيل الخروج</button>
+                </form>
 
-            <div class="col-md-4 col-sm-6">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h5 class="card-title">تعديل  المندوب </h5>
-                        <a href="#" class="btn btn-primary">تعديل </a>
-                    </div>
-                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </header>
 
-        <!-- Card with a Table -->
-        <div class="card">
-            <div class="card-header">
-                <h5 class="card-title">دورة المساعدات</h5>
-            </div>
-            <div class="card-body">
-                <!-- Responsive table inside card -->
-                <div class="table-responsive">
-                    <table class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>الاسم</th>
-                                <th>نوع المساعدة</th>
-                                <th>التاريخ </th>
-                                <th>المندوب</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+      <div class="page-wrapper">
+        <!-- Page header -->
 
-                            @forelse ($beneficial->deliveryRecordBeneficials as $item)
-                                <tr>
-                                    <td>1</td>
-                                    <td>{{ $item->id }}</td>
-                                    <td>{{ $item->product->name ?? 'No product available' }}</td>
-                                    <td>{{ $item->created_at->format('Y-m-d') }}</td>
-                                    <td>{{ $beneficial->actor->name ?? 'No actor available' }}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="5" class="text-center">لم يتم العثور على سجلات التسليم</td>
-                                </tr>
-                            @endforelse
-                        </tbody>
+        <!-- Page body -->
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row row-deck row-cards">
 
+              <div class="col-12">
+                <div class="card card-md">
+                  <div class="card-stamp card-stamp-lg">
+
+                  </div>
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col-10">
+                        <h3 class="h1">أرشادات</h3>
+                        <div class="markdown text-secondary">
+                          <div class="alert alert-success">
+                              اضافة معلومات الابناء <a href="#" class="alert-link">اضغط هنا </a>!
+                          </div>
+                          <div class="alert alert-success">
+                            تعديل البيانات — <a href="{{ route('dashboard.user.index') }}" class="alert-link">اضغط هنا</a>!
+                          </div>
+                          <div class="alert alert-success">
+                            تعديل المندوب — <a href="#" class="alert-link">اضغط هنا</a>!
+                          </div>
+                        </div>
+                        <!-- <div class="mt-3">
+                          <a href="" class="btn btn-primary" target="_blank" rel="noopener">اضافة الأبناء</a>
+                          <a href="" class="btn btn-primary" target="_blank" rel="noopener">اضافة الأبناء</a>
+                          <a href="" class="btn btn-primary" target="_blank" rel="noopener">اضافة الأبناء</a>
+                        </div> -->
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">دورة المساعدات </h3>
+                  </div>
+
+                  <div class="table-responsive">
+                    <table class="table card-table table-vcenter text-nowrap datatable">
+                      <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>الاسم</th>
+                            <th>نوع المساعدة</th>
+                            <th>التاريخ </th>
+                            <th>المندوب</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        @forelse ($beneficial->deliveryRecordBeneficials as $item)
+                        <tr>
+                            <td>£</td>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->product->name ?? 'No product available' }}</td>
+                            <td>{{ $item->created_at->format('Y-m-d') }}</td>
+                            <td>{{ $beneficial->actor->name ?? 'No actor available' }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">لم يتم العثور على سجلات التسليم</td>
+                        </tr>
+                    @endforelse
+
+                      </tbody>
                     </table>
+                  </div>
+
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+        <footer class="footer footer-transparent d-print-none">
+          <div class="container-xl">
+            <div class="row text-center align-items-center flex-row-reverse">
+              <div class="col-lg-auto ms-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
 
-
+                </ul>
+              </div>
+              <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                  <li class="list-inline-item">
+                    <a href="https//ksach.org" class="link-secondary"> Saudi Center for Culture and Heritage</a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="https//ksach.org" class="link-secondary" rel="noopener">
+                      v1.0.0
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
 
-    <!-- Bootstrap JS and Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-</body>
+    <!-- Tabler Core -->
+    <script src="{{ asset('frontend/dist/js/tabler.min.js?1692870487') }}" defer></script>
+    <script src="{{ asset('frontend/dist/js/demo.min.js?1692870487') }}" defer></script>
+
+  </body>
 </html>
